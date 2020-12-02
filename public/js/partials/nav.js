84df53dff32a0145217104ef2d205fea14c8dd7a
1vanbrav0 <ivanbravopersonal@gmail.com>
2020-12-01 17:32:21 -0700

    Project almost done

--- public/js/partials/nav.js
+++ public/js/partials/nav.js
@@ -1,34 +1,46 @@
- function nav_ready(){ //navigation functions
-  document.getElementById("button_toggle_menu").addEventListener("click", function(){
-    document.getElementById("container_nav").classList.toggle("container_nav_active");
-    document.getElementsByTagName("header")[0].classList.add("active-content-transition");
-    document.getElementsByTagName("main")[0].classList.add("active-content-transition");
-    document.getElementsByTagName("footer")[0].classList.add("active-content-transition");
-    document.getElementById("background_effect_menu").classList.add("background_menu_active");
-  });
+ function close_nav() {
+   document.getElementById("container_nav").classList.toggle("container_nav_active");
+   document.getElementsByTagName("header")[0].classList.remove("active-content-transition");
+   document.getElementsByTagName("main")[0].classList.remove("active-content-transition");
+   document.getElementsByTagName("footer")[0].classList.remove("active-content-transition");
+   document.getElementById("background_effect_menu").classList.remove("background_menu_active");
+ }
 
-  document.getElementById("button_close_menu").addEventListener("click", function(){
-    document.getElementById("container_languages_list").classList.remove("lang_active");
-    document.getElementById("container_nav").classList.toggle("container_nav_active");
-    document.getElementsByTagName("header")[0].classList.remove("active-content-transition");
-    document.getElementsByTagName("main")[0].classList.remove("active-content-transition");
-    document.getElementsByTagName("footer")[0].classList.remove("active-content-transition");
-    document.getElementById("background_effect_menu").classList.remove("background_menu_active");
-  });
+ function nav_ready() { //navigation functions
+   document.getElementById("button_toggle_menu").addEventListener("click", function () {
+     document.getElementById("container_nav").classList.toggle("container_nav_active");
+     document.getElementsByTagName("header")[0].classList.add("active-content-transition");
+     document.getElementsByTagName("main")[0].classList.add("active-content-transition");
+     document.getElementsByTagName("footer")[0].classList.add("active-content-transition");
+     document.getElementById("background_effect_menu").classList.add("background_menu_active");
+   });
 
-  document.getElementById("lang_es").addEventListener("click", function(){
-    window.location.href = "/es";
-  });
 
-  document.getElementById("lang_en").addEventListener("click", function(){
-    window.location.href = "/en";
-  });
+   document.getElementById("button_close_menu").addEventListener("click", function () {
+     if (document.getElementById("container_languages_list").classList.contains("lang_active")) {
+       document.getElementById("container_languages_list").classList.remove("lang_active");
+       setTimeout(function () {
+         close_nav();
+       }, 500);
+     } else {
+       close_nav();
+     }
 
-  document.getElementById("lang_pt").addEventListener("click", function(){
-    window.location.href = "/pt";
-  });
+   });
 
-  document.getElementById("nav_container_languages_drop_down").addEventListener("click", function(){
-    document.getElementById("container_languages_list").classList.toggle("lang_active");
-  });
-}
+   document.getElementById("lang_es").addEventListener("click", function () {
+     window.location.href = "/es";
+   });
+
+   document.getElementById("lang_en").addEventListener("click", function () {
+     window.location.href = "/en";
+   });
+
+   document.getElementById("lang_pt").addEventListener("click", function () {
+     window.location.href = "/pt";
+   });
+
+   document.getElementById("nav_container_languages_drop_down").addEventListener("click", function () {
+     document.getElementById("container_languages_list").classList.toggle("lang_active");
+   });
+ }
